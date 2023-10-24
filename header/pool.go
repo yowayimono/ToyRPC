eb86ae5141437f0b66962dc0ea864d6ff480de97
yowayimono <3485000346@qq.com>
2023-10-24 16:19:14 +0800

    header commit

--- header/pool.go
+++ header/pool.go
@@ -1,1 +1,17 @@
 package header
+
+import "sync"
+
+var (
+	RequestPool  sync.Pool
+	ResponsePool sync.Pool
+)
+
+func init() {
+	RequestPool = sync.Pool{New: func() interface{} {
+		return &RequestHeader{}
+	}}
+	ResponsePool = sync.Pool{New: func() interface{} {
+		return &ResponseHeader{}
+	}}
+}
