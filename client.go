6fdb2c9b6d4ba5b2f2b150c101ec8de239fc59ea
yowayimono <3485000346@qq.com>
2023-10-24 16:23:51 +0800

    last

--- client.go
+++ client.go
@@ -1,1 +1,52 @@
 package ToyRPC
+
+import (
+	"github.com/yowayimono/ToyRPC/codec"
+	"github.com/yowayimono/ToyRPC/compressor"
+	"github.com/yowayimono/ToyRPC/serializer"
+	"io"
+	"net/rpc"
+)
+
+type Client struct {
+	*rpc.Client
+}
+
+type Option func(o *options)
+
+type options struct {
+	compressType compressor.CompressType
+	serializer   serializer.Serializer
+}
+
+func WithCompress(c compressor.CompressType) Option {
+	return func(o *options) {
+		o.compressType = c
+	}
+}
+
+func WithSerializer(serializer serializer.Serializer) Option {
+	return func(o *options) {
+		o.serializer = serializer
+	}
+}
+
+func NewClient(conn io.ReadWriteCloser, opts ...Option) *Client {
+	options := options{
+		compressType: compressor.Raw,
+		serializer:   serializer.Proto,
+	}
+	for _, option := range opts {
+		option(&options)
+	}
+	return &Client{Client: rpc.NewClientWithCodec(
+		codec.NewClientCodec(conn, options.compressType, options.serializer))}
+}
+
+func (c *Client) Call(serviceMethod string, args interface{}, reply interface{}) error {
+	return c.Client.Call(serviceMethod, args, reply)
+}
+
+func (c *Client) AsyncCall(serviceMethod string, args interface{}, reply interface{}) chan *rpc.Call {
+	return c.Go(serviceMethod, args, reply, nil).Done
+}
--- server.go
+++ server.go
@@ -1,1 +1,50 @@
 package ToyRPC
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+
+	"github.com/yowayimono/ToyRPC/codec"
+	"github.com/yowayimono/ToyRPC/serializer"
+)
+
+// Server rpc server based on net/rpc implementation
+type Server struct {
+	*rpc.Server
+	serializer.Serializer
+}
+
+// NewServer Create a new rpc server
+func NewServer(opts ...Option) *Server {
+	options := options{
+		serializer: serializer.Proto,
+	}
+	for _, option := range opts {
+		option(&options)
+	}
+
+	return &Server{&rpc.Server{}, options.serializer}
+}
+
+// Register register rpc function
+func (s *Server) Register(rcvr interface{}) error {
+	return s.Server.Register(rcvr)
+}
+
+// RegisterName register the rpc function with the specified name
+func (s *Server) RegisterName(name string, rcvr interface{}) error {
+	return s.Server.RegisterName(name, rcvr)
+}
+
+// Serve start service
+func (s *Server) Serve(lis net.Listener) {
+	log.Printf("tinyrpc started on: %s", lis.Addr().String())
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			continue
+		}
+		go s.Server.ServeCodec(codec.NewServerCodec(conn, s.Serializer))
+	}
+}
